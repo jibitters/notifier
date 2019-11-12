e961e5a44192c25a9abf39bba1362427d2039663
Ali Dehghani <ali.dehghani.g@gmail.com>
2019-11-12 14:33:57 +0330

    Let's Start

---
+++ src/main/kotlin/ir/jibit/notifier/util/Loggers.kt
@@ -0,0 +1,18 @@
+package ir.jibit.notifier.util
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+/**
+ * Provides a less horrifying syntax to define a [Logger]. For example, instead of the:
+ * ```
+ *     val log = LoggerFactory.getLogger(User::class.java)
+ * ```
+ * You can simply write:
+ * ```
+ *     val log = logger<User>()
+ * ```
+ *
+ * @author Ali Dehghani
+ */
+inline fun <reified T> logger(): Logger = LoggerFactory.getLogger(T::class.java)
---
+++ src/main/kotlin/ir/jibit/notifier/util/Throwables.kt
@@ -0,0 +1,28 @@
+package ir.jibit.notifier.util
+
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.PrintStream
+
+private const val EMPTY_STACKTRACE = ""
+
+/**
+ * Converts the given `throwable` to its corresponding full stacktrace.
+ *
+ * @return The full stacktrace for the given exception.
+ *
+ * @author Ali Dehghani
+ */
+fun Throwable?.stackTrace(): String {
+    if (this == null) return EMPTY_STACKTRACE
+    try {
+        ByteArrayOutputStream().use { baos ->
+            PrintStream(baos).use { stream ->
+                printStackTrace(stream)
+                return "$message\n$baos"
+            }
+        }
+    } catch (e: IOException) {
+        return EMPTY_STACKTRACE
+    }
+}
